4a45f0cb8114eb41ff7f3b84138af377571ab26e
oreg-kh <60749041+oreg-kh@users.noreply.github.com>
2026-02-22 21:24:33 +0100

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,10 @@
+window.BTB_CONFIG = {
+  BOT_NAME: "Brian the Barbarian",
+  DISCORD_INVITE_URL: "https://discord.com/oauth2/authorize?...",
+  SUPPORT_SERVER_URL: "https://discord.gg/....",
+
+  // ================================================================
+  // bot avatar
+  // ================================================================
+  BOT_AVATAR_URL: "assets/images/bot-avatar.png",
+};
